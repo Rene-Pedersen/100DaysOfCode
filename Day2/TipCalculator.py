3b0e023b5802cfe75a4a33c3abe11a8976cb36c2
René Pedersen <rene@pedersen.aero>
2022-01-28 15:48:09 +0400

    Day 2 project: Tip calculator

---
+++ Day2/TipCalculator.py
@@ -0,0 +1,14 @@
+# This is the final project of day 2, a tip calculator
+print("Welcome to the tip calculator.")
+
+bill = float(input("What was the toal bill? $"))
+tip_percentage = (int(input("What percentage tip would you like to give? "
+                             "10, 12 or 15? "))) / 100 + 1
+num_people = int(input("How many people to split the bill? "))
+each_to_pay = round((bill * tip_percentage) / num_people, 2)
+
+print(f"Each person should pay: ${each_to_pay}")
+
+
+
+
