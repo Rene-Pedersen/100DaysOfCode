888f156074d06ca61e932f32a3d2a821f2047f4d
René Pedersen <rene@pedersen.aero>
2022-01-30 14:34:33 +0400

    Day 3 Ex 3: Leap year checker

---
+++ Day3/Ex3.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+# divisible by 4, not 100 but 400
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
