c378afbe24749869fd1e17ddd3cb654934b27cb6
René Pedersen <rene@pedersen.aero>
2022-01-30 16:08:30 +0400

    Day 3 Ex 5: Love Calculator

---
+++ Day3/Ex5.py
@@ -0,0 +1,26 @@
+# 🚨 Don't change the code below 👇
+import itertools
+
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+true_count = 0
+for i in "true":
+    true_count += name1.lower().count(i) + name2.lower().count(i)
+
+love_count = 0
+for j in "love":
+    love_count += name1.lower().count(j) + name2.lower().count(j)
+
+score = int(str(true_count)+str(love_count))
+
+if score < 10 or score > 90:
+    print(f"Your score is {score}, you go together like coke and mentos.")
+elif 40 < score < 50:
+    print(f"Your score is {score}, you are alright together.")
+else:
+    print(f"Your score is {score}.")
