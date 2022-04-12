6f19f2fb1e02a5442fd3d63cfdc229b2674864c7
René Pedersen <rene@pedersen.aero>
2022-04-12 10:21:37 +0400

    Day 9 Ex 2: Add empty dict and loop over student_scores adding grades using name as key

--- Day9/Ex2.py
+++ Day9/Ex2.py
@@ -8,10 +8,18 @@ student_scores = {
 # 🚨 Don't change the code above 👆
 
 # TODO-1: Create an empty dictionary called student_grades.
-
+student_grades = {}
 
 # TODO-2: Write your code below to add the grades to student_grades.👇
-
+for student_name in student_scores:
+    if student_scores[student_name] <= 70:
+        student_grades[student_name] = "Fail"
+    elif student_scores[student_name] <= 80:
+        student_grades[student_name] = "Acceptable"
+    elif student_scores[student_name] <= 90:
+        student_grades[student_name] = "Exceeds Expectations"
+    elif student_scores[student_name] <= 100:
+        student_grades[student_name] = "Outstanding"
 
 # 🚨 Don't change the code below 👇
 print(student_grades)
