2aec6a9c548196d4094f27770bb36601c3a402fa
René Pedersen <rene@pedersen.aero>
2022-04-13 12:11:04 +0400

    Day 10: Calculator Add basic functions that return result of the operation

---
+++ Day10/Calculator.py
@@ -0,0 +1,19 @@
+from Art import logo
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+print(logo)
