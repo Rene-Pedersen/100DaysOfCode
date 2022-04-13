c4791379eb91cfa6c8667ff3f8d85881a8f16569
René Pedersen <rene@pedersen.aero>
2022-04-13 11:20:27 +0400

    Day 10: Add a docstring to format_name()

--- Day10/Ex1.py
+++ Day10/Ex1.py
@@ -1,8 +1,8 @@
 # Functions with output
 
 def format_name(f_name, l_name):
+    """ Takes 2 parameters and returns them in title case"""
     #full_name = f"{f_name} {l_name}".title()
     return f"{f_name} {l_name}".title()
-
 
 print(format_name(f_name="rene", l_name="PEDERSEN"))
