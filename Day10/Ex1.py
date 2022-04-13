c13a8867cd2e0ff977124fb3f4e4aef3713df861
René Pedersen <rene@pedersen.aero>
2022-04-13 11:01:27 +0400

    Day 10: Basic function return

---
+++ Day10/Ex1.py
@@ -0,0 +1,8 @@
+# Functions with output
+
+def format_name(f_name, l_name):
+    #full_name = f"{f_name} {l_name}".title()
+    return f"{f_name} {l_name}".title()
+
+
+print(format_name(f_name="rene", l_name="PEDERSEN"))
