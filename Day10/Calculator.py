e01e434d16c825fd73dae2f7e2f001a2ba2070b1
René Pedersen <rene@pedersen.aero>
2022-04-13 14:03:44 +0400

    Day 10: Calculator Let user calculate as many times as they want

--- Day10/Calculator.py
+++ Day10/Calculator.py
@@ -1,5 +1,6 @@
 from Art import logo
 
+
 def add(a, b):
     return a + b
 
@@ -16,4 +17,34 @@ def divide(a, b):
     return a / b
 
 
-print(logo)
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number? "))
+    for symbol in operations:
+        print(symbol)
+
+    done = False
+    while not done:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number? "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        want_more = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation.: ")
+        if want_more == 'y':
+            num1 = answer
+        elif want_more == 'n':
+            print("\n"*80)
+            calculator()
+        else:
+            break
+
+
+calculator()
